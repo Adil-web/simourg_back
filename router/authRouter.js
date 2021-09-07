7e163664bf35c4b7791b607bf9e3054f95b21222
Adil-web <englishm224@gmail.com>
2021-09-07 16:30:07 +0600

    commit

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,10 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('../controller/authController')
-const authMiddleware = require('../middleware/authMiddleware')
+// const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/login', controller.login)
 router.post('/user', controller.getUser)
-router.post('/users', authMiddleware, controller.getUsers)
+router.post('/users', controller.getUsers)
 
 module.exports = router
