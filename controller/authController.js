2184d89c7d80baf41f053ebf4f40baf33442e548
Adil_Zakarin <62048169+Adil-web@users.noreply.github.com>
2021-09-07 16:04:43 +0600

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -148,7 +148,7 @@ class authController {
             axios.post('https://bpm.atameken-agro.com/api/', xmlCreateObject, config)
                 .then(async response => {
                     dataXml = parser.parse(response.data, options)
-                    dataXml = dataXml.sbapi.body.response.users.user
+                    dataXml = dataXml.sbapi.body.response.user.user
                     res.json(dataXml)
                 }).catch(e => console.log(e))
         } catch (e) {
